eeb7d34b4885ae4ee1b1d266f1a66e4d97c121de
clnsmth <colin.smith@wisc.edu>
2019-07-25 11:28:34 -0400

    Create list_creators()

---
+++ R/list_creators.R
@@ -0,0 +1,26 @@
+#' List software creators
+#'
+#' @param json
+#'   (list) Software metadata in JSON format
+#'
+#' @return
+#'   (list) Software creators
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' json <- get_json('http://imcr.ontosoft.org/repository/software')
+#' list_creators(json)
+#' }
+#'
+list_creators <- function(json){
+  return(
+    lapply(
+      seq_along(json),
+      function(x){
+        json[[x]]$value[['http://ontosoft.org/software#hasCreator']]$label
+      }
+    )
+  )
+}
