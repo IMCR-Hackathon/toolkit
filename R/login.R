443ae30e2f977978d1a5ffa6f310fbcbbfbfd873
clnsmth <colin.smith@wisc.edu>
2019-08-21 20:51:22 -0400

    Create login()
    A function to login and get the session string.

---
+++ R/login.R
@@ -0,0 +1,43 @@
+#' Login
+#'
+#' @usage
+#'   login(user.name = NULL, user.pass = NULL)
+#'
+#' @param user.name
+#'   (character) OntoSoft user name. Default (NULL) queries for this
+#'   information in the console.
+#' @param user.pass
+#'   (character) OntoSoft user password. Default (NULL) queries for this
+#'   information in the console.
+#'
+#' @return
+#'   (character) Session string
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Enter credentials to function
+#' login('myname', 'mypass')
+#' # Have credentials requested in the console
+#' login()
+#' }
+#'
+login <- function(user.name = NULL, user.pass = NULL){
+
+  if (any(is.null(user.name), is.null(user.pass))){
+    user.name <- readline('Enter user name: ')
+    user.pass <- readline('Enter user password: ')
+  }
+
+  r <- httr::POST(
+    url = 'http://imcr.ontosoft.org/repository/login',
+    body = list(name = user.name, password = user.pass),
+    encode = 'json')
+
+  sstr <- jsonlite::fromJSON(
+    httr::content(r, as = 'text', encoding = 'UTF-8'))$sessionString
+
+  return(sstr)
+
+}
