bc9fd11c5928589f52062ffa948a0c3680aad68f
clnsmth <colin.smith@wisc.edu>
2019-08-23 21:31:29 -0400

    Default to IMCR URL

--- R/get_json.R
+++ R/get_json.R
@@ -13,7 +13,7 @@
 #' json <- get_json("http://imcr.ontosoft.org/repository/software")
 #' }
 #'
-get_json <- function(url){
+get_json <- function(url = "http://imcr.ontosoft.org/repository/software"){
   ids <- jsonlite::fromJSON(url)$id
   json <- lapply(ids, jsonlite::fromJSON)
   names(json) <- unlist(
