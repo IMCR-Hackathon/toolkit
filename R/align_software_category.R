c1d57711d3f76144ea4f0dfce57a24368a1c86c8
Colin Smith <colin.smith@wisc.edu>
2019-08-24 17:56:42 -0500

    Create align_software_category()
    A function to align keywords with the IMCR Controlled Vocabulary.

---
+++ R/align_software_category.R
@@ -0,0 +1,101 @@
+#' Align software category keywords with the IMCR vocabulary
+#'
+#' @param software
+#'   (character) Software name
+#'
+#' @return
+#'   (list) Updated software JSON as a list object.
+#'   (.json file) Updated software JSON written to the IMCR Portal, and to
+#'   \code{paste0(tempdir(), "/", software, ".json")}.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Get all IMCR JSON
+#' get_imcr_json()
+#' 
+#' # Login
+#' login()
+#' 
+#' # Add software category keywords
+#' update_software_category("add", 'arrow', c('quality control', 'import'))
+#' 
+#' # Remove software category keywords
+#' update_software_category("remove", 'arrow', c('quality control', 'import'))
+#' }
+#'
+align_software_category <- function(software){
+  
+  # Check for session string
+  if (!exists("imcr_session_string")) {
+    stop(
+      paste0(
+        "The object 'imcr_session_string' is missing from the global environment.",
+        "Create it with 'login()."
+      )
+    )
+  }
+  
+  # Check for imcr_json object
+  if (!exists("imcr_json") | !is.list(imcr_json)) {
+    stop(
+      paste0(
+        "The object 'imcr_json' is missing from the global environment.",
+        "Create it with 'get_json()."
+      )
+    )
+  }
+  
+  # Get software categories
+  json <- imcr_json[names(imcr_json) == software]
+  cats <- json[[1]]$value[['http://ontosoft.org/software#hasSoftwareCategory']]
+  
+  
+  # Add/remove user specified keywords
+  if (action == 'add') {
+    
+    newcats <- data.frame(
+      rep('EnumerationEntity', length(keywords)),
+      rep('', length(keywords)),
+      rep('', length(keywords)),
+      rep('http://ontosoft.org/software#SoftwareCategory', length(keywords)),
+      keywords,
+      keywords,
+      stringsAsFactors = FALSE
+    )
+    names(newcats) <- c("@type", "id", "name", "type", "label", "value")
+    cats <- rbind(cats, newcats)
+    json[[1]]$value[['http://ontosoft.org/software#hasSoftwareCategory']] <- cats
+    
+  } else if (action == 'remove') {
+    
+    newcats <- cats
+    newcats <- newcats[newcats$label != keywords]
+    if (all(dim(newcats) == c(2,0))) {
+      json[[1]]$value[['http://ontosoft.org/software#hasSoftwareCategory']] <- list()
+    } else {
+      json[[1]]$value[['http://ontosoft.org/software#hasSoftwareCategory']] <- newcats
+    }
+    
+  }
+  
+  # Add/remove broader keywords
+  
+  # Write to file
+  jsonlite::write_json(
+    json[[1]],
+    path = paste0(tempdir(), "/", software, ".json"),
+    auto_unbox = TRUE
+  )
+  
+  # Upload
+  put_software(
+    software, 
+    imcr_session_string, 
+    paste0(tempdir(), "/", software, ".json")
+  )
+  
+  return(json[[1]])
+  
+}
